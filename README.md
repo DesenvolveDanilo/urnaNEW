0296270bb4bd7bf5d589ca60e8ca587c5b472b26
Danilo jesus <danilocuencas@hotmail.com>
2022-04-02 14:42:37 -0300

    v1 urna

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Urna Eletrônica 
+
+
+Olá, em clima de eleição e compa do mundo foi feito uma réplica da nossa famosa urna
---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    canditados: [
+      {
+        numero: 38897,
+        nome: "funaldo de coisa",
+        partido: "não podemos",
+        fotos: [
+          {
+            url: "https://i.pinimg.com/originals/b4/e3/80/b4e380db4a55647a322df8761ed0e519.jpg",
+            legenda: "vereador",
+            small: false,
+          },
+        ],
+      },
+      {
+        numero: 38897,
+        nome: "funaldo de hoje",
+        partido: "talvez podemos",
+        fotos: [
+          {
+            url: "https://i.pinimg.com/originals/b4/e3/80/b4e380db4a55647a322df8761ed0e519.jpg",
+            legenda: "vereador",
+            small: false,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    canditados: [
+      {
+        numero: 12,
+        nome: "Robnisto",
+        partido: "robamos",
+        vice: "cic",
+        fotos: [
+          { url: "...", legenda: "Prefeito" },
+          { url: "...", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: 12,
+        nome: "alfanado",
+        partido: "deitamos",
+        vice: "lindo",
+        fotos: [
+          { url: "...", legenda: "Prefeito" },
+          { url: "...", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
